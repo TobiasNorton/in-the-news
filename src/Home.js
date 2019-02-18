a889aa48fd4e452d42e76bdf17cd860937ef07e0
Tobias Norton <tnorton20@yahoo.com>
2019-02-17 21:18:06 -0500

    Buttons set news outlet preference in Home

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,8 @@ class Home extends Component {
     super(props)
 
     this.state = {
-      articles: []
+      articles: [],
+      outlet: ''
     }
   }
 
@@ -44,43 +45,73 @@ class Home extends Component {
     })
   }
 
+  getPreferredOutlet = event => {
+    this.setState(
+      {
+        outlet: event.target.dataset.domain
+      },
+      () =>
+        axios
+          .get(
+            `https://newsapi.org/v2/everything?domains=${
+              this.state.outlet
+            }&apiKey=724c68adcd604fd7bcd865950a9eddb1`
+          )
+          .then(response => {
+            console.log(response.data.articles)
+            this.setState({
+              articles: response.data.articles
+            })
+          })
+    )
+  }
+
   render() {
     return (
       <div>
-        <div className="categories-container">
-          <button onClick={this.reloadHeadlines} data-name="business">
-            BBC
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="entertainment">
-            The Atlantic
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="general">
-            Reuters
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="health">
-            NBC News
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="science">
-            Vice
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="sports">
-            IndieWire
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="business">
-            Business Insider
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="business">
-            New York Times
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="business">
-            The Wall Street Journal
-          </button>
-          <button onClick={this.reloadHeadlines} data-name="business">
-            NPR
-          </button>
-        </div>
         <h1 className="main-title">In the News</h1>
         <p className="main-caption">You heard it here or somewhere else first.</p>
+        <div className="categories-container">
+          <button onClick={this.getPreferredOutlet} data-domain="bbc.com">
+            BBC
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="theatlantic.com">
+            The Atlantic
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="reuters.com">
+            Reuters
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="nbcnews.com">
+            NBC News
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="vice.com">
+            Vice
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="indiewire.com">
+            IndieWire
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="businessinsider.com">
+            Business Insider
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="nytimes.com">
+            New York Times
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="wsj.com">
+            WSJ
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="npr.org">
+            NPR
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="cbsnews.com">
+            CBS News
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="slate.com">
+            Slate
+          </button>
+          <button onClick={this.getPreferredOutlet} data-domain="usatoday.com">
+            USA Today
+          </button>
+        </div>
         {this.showArticles()}
       </div>
     )
